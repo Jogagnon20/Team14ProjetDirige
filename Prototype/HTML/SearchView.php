5972e4e9045ad2e1c5665fe0bcefc5bd488114fc
Bankokiller <guillaume.paquin@videotron.ca>
2020-04-25 15:11:42 -0400

    Add files via upload

---
+++ Prototype/HTML/SearchView.php
@@ -0,0 +1,11 @@
+<?php
+    //include "CLASSES/SPECTACLE/spectacle.php";
+    //$spectacle_list = Spectacle::search_spectacle($_GET["search"]);
+?>
+
+<h1 class="my-4">Search results:</h1>
+<?php
+  //foreach($spectacle_list as $spectacle){
+  //  $spectacle->display_spectacle();
+  //}
+?>
