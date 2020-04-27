3ca4cbc0adb08227a6ea4f6e2d37346ebc7a43b3
Nicolasd0 <64101942+Nicolasd0@users.noreply.github.com>
2020-04-27 16:15:50 -0400

    Add files via upload

--- Prototype/HTML/registerview.php
+++ Prototype/HTML/registerview.php
@@ -1,3 +1,4 @@
+
 <div class="container" style="margin-top:30px">
     <div class="row">
         <div class="col-sm-4">
@@ -5,48 +6,126 @@
             <form method = "post" action = "./DOMAINLOGIC/register.dom.php">
                 
                 <div class="form-group">
-                    <label for="email">Email:</label>
-                    <input type="email" class="form-control" name="email" id="email" required><br>
+                    <label for="email">Courriel:</label>
+                    <?php
+                        if(isset($_SESSION['email'])){
+                            $email = $_SESSION['email'];
+                            echo "<input type='text' class='form-control' value='$email' name='email' id='email' required>";
+                        }
+                        else{
+                            echo "<input type='text' class='form-control' value='' name='email' id='email' required><br>";
+                        }
+                    ?>
+                     <?php
+                        if(isset( $_SESSION['errorEmail'])){
+                            $error = $_SESSION['errorEmail'];
+                            echo "<div style='color:red'> $error </div>";
+                        }
+                        
+                    ?>
+                    
                     <div class="valid-feedback">Valid.</div>
-                    <div class="invalid-feedback">Please fill out this field.</div>
+                    <div class="invalid-feedback">Veuillez remplir ce champs.</div>
                 </div>
 
                 <div class="form-group">
-                        <label for="username">username:</label>
-                        <input type="text" class="form-control" name="username" id="username" required><br>
+                        <label for="name">Nom:</label>
+                        <?php
+                        if(isset($_SESSION['name'])){
+                            $name = $_SESSION['name'];
+                            echo "<input type='text' class='form-control' value='$name' name='name' id='name' required>";
+                        }
+                        else{
+                            echo "<input type='text' class='form-control' value='' name='name' id='name' required><br>";
+                        }
+                    ?>
                         <div class="valid-feedback">Valid.</div>
-                        <div class="invalid-feedback">Please fill out this field.</div>
+                        <div class="invalid-feedback">Veuillez remplir ce champs.</div>
                 </div>
 
                 <div class="form-group">
-                        <label for="pwd">Password:</label>
+                        <label for="pwd">Mot de passe:</label>
                         <input type="password" class="form-control" name="pw" id="pwd" required><br>
+                        <?php
+                        if(isset( $_SESSION['errorPassword'])){
+                            $password = $_SESSION['errorPassword'];
+                            echo "<div style='color:red'> $password </div>";
+                        }
+                        
+                    ?>
                         <div class="valid-feedback">Valid.</div>
-                        <div class="invalid-feedback">Please fill out this field.</div>
+                        <div class="invalid-feedback">Veuillez remplir ce champs.</div>
                 </div>
 
                 <div class="form-group">
-                    <label for="pwd">password validation:</label>
+                    <label for="pwd">Validation mot de passe:</label>
                     <input type="password" class="form-control" name="pwValidation" id="pwValidation" required><br>
                     <div class="valid-feedback">Valid.</div>
-                    <div class="invalid-feedback">Please fill out this field.</div>
+                    <div class="invalid-feedback">Veuillez remplir ce champs.</div>
                 </div>
 
                 <div class="form-group">
                         <label for="adress">Adresse:</label>
-                        <input type="adress" class="form-control" name="adress" id="adress" required><br>
+                        <?php
+                        if(isset($_SESSION['adress'])){
+                            $adress = $_SESSION['adress'];
+                            echo "<input type='text' class='form-control' name='adress' value='$adress' id='adress' required>";
+                        }
+                        else{
+                            echo "<input type='text' class='form-control' name='adress' id='adress' required><br>";
+                        }
+                    ?>
+                        
                         <div class="valid-feedback">Valid.</div>
-                        <div class="invalid-feedback">Please fill out this field.</div>
+                        <div class="invalid-feedback">Veuillez remplir ce champs.</div>
+                </div>
+
+                <div class="form-group">
+                        <label for="phone">Téléphone:</label>
+                        <?php
+                        if(isset($_SESSION['phone'])){
+                            $phone = $_SESSION['phone'];
+                            echo "<input type='text' class='form-control' value='$phone' name='phone' id='phone' placeholder='514-123-1234' required>";
+                        }
+                        else{
+                            echo "<input type='text' class='form-control' value='' name='phone' id='phone' required placeholder='514-123-1234'><br>";
+                        }
+                    ?>
+                    <?php
+                        if(isset( $_SESSION['errorPhone'])){
+                            $phone = $_SESSION['errorPhone'];
+                            echo "<div style='color:red'> $phone </div>";
+                        }
+                        
+                    ?>
+                        <div class="valid-feedback">Valid.</div>
+                        <div class="invalid-feedback">Veuillez remplir ce champs.</div>
                 </div>
 
                 <div class="form-group">
                         <label for="PostalCode">Code Postal:</label>
-                        <input type="PostalCode" class="form-control" name="pc" id="PostalCode" required><br>
+                        <?php
+                        if(isset($_SESSION['pc'])){
+                            $pc = $_SESSION['pc'];
+                            echo "<input type='text' class='form-control' value='$pc' name='pc' id='pc' required>";
+                        }
+                        else{
+                            echo "<input type='text' class='form-control' value='' name='pc' id='pc' required><br>";
+                        }
+                    ?>
+                     <?php
+                        if(isset( $_SESSION['errorPc'])){
+                            $pc = $_SESSION['errorPc'];
+                            echo "<div style='color:red'> $pc </div>";
+                        }
+                        
+                    ?>
                         <div class="valid-feedback">Valid.</div>
-                        <div class="invalid-feedback">Please fill out this field.</div>
+                        <div class="invalid-feedback">Veuillez remplir ce champs.</div>
                 </div>
                 <button class="btn btn-success" type="submit">Register</button>
             </form>
         </div>
     </div>
 </div>
+<script>$('#phone').mask('(000) 000-0000');</script>
