67ebe4417bb85d801b24b8abc1f6b51101380a49
Darian3107 <55796885+Darian3107@users.noreply.github.com>
2020-04-27 14:12:25 -0400

    Add files via upload

--- Prototype/DOMAINLOGIC/login.dom.php
+++ Prototype/DOMAINLOGIC/login.dom.php
@@ -9,21 +9,34 @@
         header("Location: ../error.php?ErrorMSG=already%20logged%20in!");
         die();
     }
+    if(isset($_SESSION['Client'])))
+    {
+        header("Location: ../Spectacles.php");
+        die();
+    }
 
     //prendre les variables du Post
     $email = $_POST["email"];
     $pw = $_POST["pw"];
 
-    //Validation Posts
-    //$aUser = new User(); création d'un user
+    //Validation Posts*****************************************************
 
-    //validateLogin
-    if($aUser->Login($email, $pw))
+
+    //getClient
+    $mybd = new PDO('mysql:host=167.114.152.54;dbname=dbequipe14;charset=utf8', 'equipe14', 'in6vest14');
+    $stmt = $mybd->prepare("CALL SelectFromClients()");
+    $stmt->execute();
+    while ($donnees = $stmt1->fetch())
     {
-        login($aUser->get_id(), $aUser->get_email(), $aUser->get_username());
-        header("Location: ../Spectacles.php");
-        die();
+        if($donnees['email'] == $email){
+            $aClient = new Clients($donnees['idClient'], $donnees['nomClient'], $email, $donnees['adresseClient'], $donnees['telephoneClient']);
+        }
     }
+    $mybd = null;
+    $_SESSION['Client'] = $aClient;
     
-    header("Location: ../error.php?ErrorMSG=invalid email or password");
+
+
+    header("Location: ../Spectacles.php");
     die();
+?>
