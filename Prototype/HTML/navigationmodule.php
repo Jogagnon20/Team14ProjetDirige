b2122d629499a1644768d2ece89d592c365580c3
Jogagnon20 <58305873+Jogagnon20@users.noreply.github.com>
2020-04-28 15:30:45 -0400

    Add files via upload

--- Prototype/HTML/navigationmodule.php
+++ Prototype/HTML/navigationmodule.php
@@ -3,7 +3,7 @@
     if(isset($_SESSION["Client"])){
         $navLinks = '
         <li class="nav-item">
-            <a class="nav-link" href="DOMAINLOGIC/logout.dom.php">Logout</a>
+            <a class="nav-link" href="DOMAINLOGIC/logout.dom.php">Déconnexion</a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="Mon Profile.php">Mon Profile</a>
@@ -13,10 +13,10 @@
     else{
     $navLinks = '
     <li class="nav-item">
-        <a class="nav-link" href="login.php">Login</a>
+        <a class="nav-link" href="login.php">Connexion</a>
     </li>
     <li class="nav-item">
-        <a class="nav-link" href="register.php">Register</a>
+        <a class="nav-link" href="register.php">Inscription</a>
     </li>';
     }
 ?>
@@ -33,11 +33,17 @@
             <input class="form-control mr-sm-2" type="text" placeholder="Search" name="search">
             <select name="RechercheAvance" class="form-control">
                 <option value="NomSpectacle" selected>Nom du Spectacle</option>
-                <option value="Categorie">Categorie</option>
                 <option value="NomArtiste">Nom de l'artiste</option>
                 <option value="Description">Description</option>
+                <optgroup label="Categorie">
+                    <option value="Categorie|HUM">Humour</option>
+                    <option value="Categorie|DAN">Dance</option>
+                    <option value="Categorie|THE">Théâtre</option>
+                    <option value="Categorie|MAG">Magie</option>
+                    <option value="Categorie|MUS">Musique</option>
+                </optgroup>
             </select>
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Recherche</button>
         </form>
         <ul class="navbar-nav mr-auto, text-right">
             <?php
