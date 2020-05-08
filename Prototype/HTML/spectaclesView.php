3dfd589ced941bcd494626322ade8452eea9ecc7
Bankokiller <guillaume.paquin@videotron.ca>
2020-05-08 15:00:53 -0400

    Add files via upload

--- Prototype/HTML/spectaclesView.php
+++ Prototype/HTML/spectaclesView.php
@@ -5,6 +5,8 @@
 		$Spectacles = array();
 		$mybd = new PDO('mysql:host=167.114.152.54;dbname=dbequipe14;charset=utf8', 'equipe14', 'in6vest14');
 		$resultat = $mybd->query("call BaseSortSpectacles");
+		echo "
+				<div class='grid-container'>";
 		while ($donnees = $resultat->fetch(PDO::FETCH_ASSOC))
 		{
 			$idCategorie = $donnees['idCategorie'];
@@ -15,52 +17,50 @@
 			$GUID = $donnees['GUID'];
 			$prix = $donnees['prix_de_base'];
 			echo "
-				<div class='grid-container'>
-					<div style='grid-area: Picture;'>
-						<a href='DetailSpectacle.php?id=$id'><img class='rounded' width='304' height='236' src='Images/$GUID' alt='$titre'></a>
+				<div style='display:grid; grid-template-columns: 60% 40%; padding-left:10px'>
+					<div>
+						<div>
+							<b style='font-size:30px; align-content:center'>
+								$titre
+							</b> 
+							<a href='Achats.php' style='float:right'>
+								<button >
+									Achat
+								</button></a>
+						</div>
+						<a href='DetailSpectacle.php?id=$id'>
+							<img class='rounded' width='304' height='236' src='Images/$GUID' alt='$titre'>
+						</a>
 					</div>
-					<div style='grid-area: title;'>
-						<span style='font-size:30px; align-content:center'><b>$titre</b></span>
-						<a href='Achats.php'>Achat</a>
-					</div>
-					<div style='grid-area: description;'>
-					<span>Fait par: $artiste</span><br>
-					<span>Prix de base: $prix$</span><br>  
-					<span>Categorie: ";
-						switch($idCategorie){
-							case 'HUM':
-								echo 'Humour';
-								break;
-							case 'MAG':
-								echo 'Magie';
-								break;
-							case 'DAN':
-								echo 'Danse';
-								break;
-							case 'MUS':
-								echo 'Musique';
-								break;
-							case 'DRA':
-								echo 'Drame';
-								break;
-						}
+					<div style='font-size:22px'>
+					<br>
+					<br>
+					<br>
+						<span>Fait par: $artiste</span><br>
+						<span>Prix de base: $prix$</span><br>  
+						<span>Categorie: ";
+							switch($idCategorie){
+								case 'HUM':
+									echo 'Humour';
+									break;
+								case 'MAG':
+									echo 'Magie';
+									break;
+								case 'DAN':
+									echo 'Danse';
+									break;
+								case 'MUS':
+									echo 'Musique';
+									break;
+								case 'DRA':
+									echo 'Drame';
+									break;
+							}
 					echo "</span>
 					</div>
-						<div style='grid-area: Salles;'>
-						<span>Il prendra place au:<br>
-						<ul>";
-						foreach(GetSallesSpectacles($id) as $item){
-							echo "<li>$item </li>";
-						}
-						echo" </ul></span><br> 
-					</div>
-				</div>
-				<div class='hiddenDiv' id='spectacle$id'>
-					Détails spectacle
-					<button><a href='panier.php?id=spectacle$id'>Acheter</a></button>
-				</div>
-				<hr>";
+					</div>";
 		}
+		echo "</div>";
 		$resultat->closeCursor();
 	}
 	catch (PDOException $e)
