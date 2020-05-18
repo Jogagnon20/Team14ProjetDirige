aea637d4f4fbd58b5c01d0ab527e084570e09d2a
Jogagnon20 <58305873+Jogagnon20@users.noreply.github.com>
2020-05-18 15:13:00 -0400

    Add files via upload

--- Prototype/DOMAINLOGIC/profil.dom.php
+++ Prototype/DOMAINLOGIC/profil.dom.php
@@ -1,22 +1,32 @@
 <?php
+    include "../DAL/models.php";
+    session_start();
+
+    $email = $_GET['courrielClient'];
+    $Newpassword = $_GET['newPassword'];
+    $verifPassword = $_GET['verifPassword'];
+
+
+    var_dump($_GET);
     $mybd = new PDO('mysql:host=167.114.152.54;dbname=dbequipe14;charset=utf8', 'equipe14', 'Prototype14');
-    $stmt1 = $mybd->prepare("CALL UpdateProfil(nomClient, adresseClient, telephoneClient, email, password)");
+    $stmt1 = $mybd->prepare("CALL UpdateProfil(id, nomClient, adresseClient, telephoneClient, email, password)");
+    $stmt1->bindParam(2, $_GET['nomClient']);
+    $stmt1->bindParam(3, $_GET['adresseClient']);
+    $stmt1->bindParam(4, $_GET['telephoneClient']);
+    $stmt1->bindParam(5, $_GET['courrielClient']);
+    $stmt1->bindParam(6, $_GET['newPassword']);
+    $stmt1->execute();
+    $stmt1 = $mybd->prepare("call SelectFromClients");
     $stmt1->execute();
     while ($donnees = $stmt1->fetch())
     {
-        if($donnees['email'] == $email){
+        if($donnees['idClient'] == $_SESSION['idClient']){
             $aClient = new Clients($donnees['idClient'], $donnees['nomClient'], $email, $donnees['adresseClient'], $donnees['telephoneClient']);
         }
     }
+
     $mybd = null;
     $_SESSION['Client'] = $aClient;
-
-    
-
-
-
-
-
 
 
 
