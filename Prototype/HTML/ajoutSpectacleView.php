6a2acc64d3b682062e77dcc4091c2fa623a0ec57
Nicolasd0 <64101942+Nicolasd0@users.noreply.github.com>
2020-05-18 17:08:08 -0400

    Add files via upload

--- Prototype/HTML/ajoutSpectacleView.php
+++ Prototype/HTML/ajoutSpectacleView.php
@@ -1,8 +1,18 @@
+<?php
+    
+require './UTILS/imageHelper.php';
+
+$imageHelper = new ImageHelper('./Images','No_image.png') ;
+
+$html_imageUpLoader = $imageHelper->html_ImageUploader('');
+$_SESSION['imageHelper'] = $imageHelper;
+?>
+
 <div class="container" style="margin-top:30px">
     <div class="row">
         <div class="col-sm-4">
             <h2>Ajout d'un spectacle</h2>
-            <form method = "post"  action = "./DOMAINLOGIC/ajoutSpectacle.dom.php">
+            <form method = "post" enctype="multipart/form-data"  action = "./DOMAINLOGIC/ajoutSpectacle.dom.php">
                 <div class="form-group">
                     
                     <label for="NomSpectacle">Nom du spectacle:</label>
@@ -52,15 +62,17 @@
                     <div id='selectDiv'>
                     </div>
                     <br>
-                    <label style="display:block"  for="affiche">Url de l'affiche</label>
-                    <input type="url" id="affiche" class='form-control' name="affiche" required>
-                    <br>
+                    <label style="display:block"  for="ImageUploader">Url de l'affiche</label>
+                    <?php echo $html_imageUpLoader; ?> <br> <br>
+                    
                     <label style="display:block"  for="description">Description du spectacle</label>
                     <textarea  id="description" class='form-control' name="description" required></textarea>
                 </div>
                
                 <button class="btn btn-success" type="submit">Ajout</button>
             </form>
+            <script src='./js/jquery-3.3.1.js'></script>        
+            <script src='./js/imageUpLoader.js'></script>
         </div>
     </div>
 </div>
@@ -104,4 +116,46 @@
         
     }
 }
+
+
+$("#ImageUploader").change(function (e) { PreLoadImage(e); });
+$("#UploadButton").click(function () { $("#ImageUploader").trigger("click"); });
+$("#UploadedImage").click(function () { $("#ImageUploader").trigger("click"); });
+$("#PDFUploadButton").click(function () { $("#PDFUploader").trigger("click"); });
+
+function PreLoadImage(e) {
+    // Saisir la référence sur l'image cible
+    var imageTarget = $("#UploadedImage")[0];
+    // Saisir la référence sur le fileUpload
+    var input = $("#ImageUploader")[0];
+
+    if (imageTarget != null) {
+        var len = input.value.length;
+
+        if (len != 0) {
+            var fname = input.value;
+            var ext = fname.substr(len - 3, len).toLowerCase();
+
+            if ((ext != "png") &&
+                (ext != "jpg") &&
+                (ext != "bmp") &&
+                (ext != "gif")) {
+                bootbox.alert("Ce n'est pas un fichier d'image de format reconnu. Sélectionnez un autre fichier.");
+            }
+            else {
+                var fReader = new FileReader();
+                fReader.readAsDataURL(input.files[0]);
+                fReader.onloadend = function (event) {
+                    // event.target.result contiens les données de l'image
+                    imageTarget.src = event.target.result;
+                }
+            }
+        }
+        else {
+            imageTarget.src = null;
+        }
+    }
+    return true;
+}
 </script>
+
